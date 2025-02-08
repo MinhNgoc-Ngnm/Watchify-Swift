94a1a3d0cb98011951da59ccb7a1a656099bdf76
ngnm1009 <ngnm1009@gmail.com>
2025-02-08 14:51:58 +0700

    Initial Commit

---
+++ Watchify-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Watchify-Swift
+//
+//  Created by Minh Ngọc on 08/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
